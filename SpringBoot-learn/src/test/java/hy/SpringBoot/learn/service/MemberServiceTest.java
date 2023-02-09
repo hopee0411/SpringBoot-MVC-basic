8eec6b7980c2c6686b320e59711296465d084eb7
hopee0411 <hopee0411@naver.com>
2023-02-09 10:51:29 +0900

    test

--- SpringBoot-learn/src/test/java/hy/SpringBoot/learn/service/MemberServiceTest.java
+++ SpringBoot-learn/src/test/java/hy/SpringBoot/learn/service/MemberServiceTest.java
@@ -50,7 +50,7 @@ class MemberServiceTest {
         //when
         memberService.join(member1);
         //단축키 컨트롤 쉬프트 v
-        IllegalStateException e =assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
         assertThat(e.getMessage()).isEqualTo("이미존재하는 회원입니다.");
 //        try {
 //            memberService.join(member2);
